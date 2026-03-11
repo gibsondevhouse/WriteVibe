5b4a519cfa2e4833b5a847935ce278046e09d6f5
Chris Gibson <gibsondevhouse@gmail.com>
2026-03-10 21:32:31 -0400

    Initial Commit

---
+++ WriteVibeTests/WriteVibeTests.swift
@@ -0,0 +1,17 @@
+//
+//  WriteVibeTests.swift
+//  WriteVibeTests
+//
+//  Created by Christopher Gibson on 3/10/26.
+//
+
+import Testing
+@testable import WriteVibe
+
+struct WriteVibeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
